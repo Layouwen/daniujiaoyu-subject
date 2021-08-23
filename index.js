fc0ec6eddc97688a5c55557e30f49554a058547f
Avan <layouwen@gmail.com>
2021-08-23 16:19:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         大牛教育忽略某学科
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://os.daniujiaoyu.org/html/member/subject
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
